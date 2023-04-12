ddeb1b6668ca29cc11fa2b60866404ed8ea06462
Dede Nugroho <this.nugroho@gmail.com>
2023-04-12 10:11:34 +0800

    Code improvement

--- src/DiscordWebhook.php
+++ src/DiscordWebhook.php
@@ -5,18 +5,39 @@ namespace Thisnugroho\DiscordWebhook;
 use Illuminate\Support\Facades\Http;
 use Spatie\DiscordAlerts\DiscordAlert;
 use Thisnugroho\DiscordWebhook\Elements\Element;
+use Thisnugroho\DiscordWebhook\Exceptions\DiscordWebhookException;
 use Thisnugroho\DiscordWebhook\Messages\Messages;
 
 class DiscordWebhook
 {
-    public function send(Element $message)
+
+    private Element $element;
+
+
+    private function getPayload(): array
     {
-        // dd($message->toPayload());
-        $request = Http::asJson()
+        return $this->element->toPayload();
+    }
+
+    private function getPayloadKeys(): array
+    {
+        return array_keys($this->getPayload());
+    }
+
+    public function send(Element $element)
+    {
+        $this->element = $element;
+        if (($fileExists = in_array('file', $this->getPayloadKeys())) && in_array('embeds', $this->getPayloadKeys())) {
+            throw new DiscordWebhookException("Embeds and files cannot be sent at once");
+        }
+
+        $baseRequest = $fileExists ? Http::asMultipart() : Http::asJson();
+
+        $request = $baseRequest
             ->post(
                 env("DISCORD_WEBHOOK"),
-                $payload = $message->toPayload()
+                $this->getPayload()
             );
-        dd($payload, $request->body());
+        return $request->body();
     }
 }
