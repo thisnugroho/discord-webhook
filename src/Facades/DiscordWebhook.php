1a90cda6c2ad6e9176857c206f7f3550a408a4af
faisalwahyudi10 <faisal.piffent.s@gmail.com>
2026-01-23 10:49:47 +0800

    feat: add default webhook fallback and support multiple webhook targets

--- src/Facades/DiscordWebhook.php
+++ src/Facades/DiscordWebhook.php
@@ -5,10 +5,14 @@ namespace Thisnugroho\DiscordWebhook\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * @see \Thisnugroho\DiscordWebhook\Skeleton\SkeletonClass
- * @method static void send(\Thisnugroho\DiscordWebhook\Elements\Element | string | array $message)
- * 
+ * @method static \Thisnugroho\DiscordWebhook\DiscordWebhook send(
+ *     \Thisnugroho\DiscordWebhook\Elements\Element|string|array $content
+ * )
+ * @method static \Thisnugroho\DiscordWebhook\DiscordWebhook to(string|array $names)
+ * @method static \Thisnugroho\DiscordWebhook\DiscordWebhook toUrl(string|array $urls)
+ * @method static \Thisnugroho\DiscordWebhook\DiscordWebhook multipart(bool $value = true)
  */
+
 class DiscordWebhook extends Facade
 {
     protected static function getFacadeAccessor(): string
